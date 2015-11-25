e740ad97dd078ced6fbe7664662a097d5651e018
Dr. Kilian Kilger <kilian.kilger@sap.com>
2015-11-25 18:46:02 +0100

    Clean up backup file

--- init/init-backup.el
+++ init/init-backup.el
@@ -10,15 +10,14 @@
 (defvar my-autosave-dir (expand-file-name "~/.emacs.d/autosave/")
   "My auto save directory.")
 
-(setq
-   backup-by-copying t      ; don't clobber symlinks
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
+(setq backup-by-copying t)    ; don't clobber symlinks
+(setq delete-old-versions t)
+(setq kept-new-versions 6)
+(setq kept-old-versions 2)
+(setq version-control t)      ; use versioned backups
 (setq vc-make-backup-files t) ; Perforce is no real version control system
-
 (setq backup-directory-alist (list (cons ".*" my-backup-dir)))
 (setq auto-save-list-file-prefix my-autosave-dir)
 (setq auto-save-file-name-transforms `((".*" ,my-autosave-dir t)))
+
 (provide 'init-backup)
