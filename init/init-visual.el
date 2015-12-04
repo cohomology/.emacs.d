4be54b037344d001242dfb0bdc7b8e3b92b41c91
Kilian Kilger <kkilger@gmail.com>
2015-12-05 00:00:15 +0100

    Postpone powerline-moe-theme

--- init/init-visual.el
+++ init/init-visual.el
@@ -5,16 +5,16 @@
 
 (setq default-frame-alist '((font . "dejavu sans mono:pixelsize=17")))
 
+(use-package powerline
+  :ensure t)
+
 (use-package moe-theme
   :config  
   (progn
     (moe-dark)
-    (moe-theme-set-color 'blue))
+    (moe-theme-set-color 'blue)
+    (powerline-moe-theme))
   :ensure t)
 
-(use-package powerline
-  :config
-  (powerline-moe-theme)
-  :ensure t)
 
 (provide 'init-visual)
