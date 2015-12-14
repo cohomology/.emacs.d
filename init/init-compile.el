38db86086fac2e14420b898dfab71813ac78b6df
Kilian Kilger <kkilger@gmail.com>
2015-12-14 19:43:17 +0100

    Remove stupid code

--- init/init-compile.el
+++ init/init-compile.el
@@ -3,12 +3,9 @@
   :group 'my-init
   :prefix 'my-init-compile)
 
-(add-to-list 'safe-local-variable-values
-             '(compile-command . "make_cgk"))
-
 ;; accept any command starting with "make" as valid compile-command
-(put 'compile-command 'safe-local-variable (lambda (x)
-                                             (and (stringp x) (string-prefix-p "make" x))))
+(put 'compile-command 'safe-local-variable (lambda (cmd)
+                                             (and (stringp cmd) (string-prefix-p "make" cmd))))
 
 (defvar-local my-init-compile/compile-common ""
   "Compile common projects. Should be overwritten by .dir-locals.el")
